168fe7cf27dcd379d1b8a7005dd2de58b4d7b776
Anthony Johnson <theanthonyjohnson@gmail.com>
2021-02-25 21:02:27 -0500

    added more files

--- server.js
+++ server.js
@@ -6,9 +6,30 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
 
+
+function handleLocation(req, res){
+  const jsonArraayNew = require('./data/location.json');
+  res.send(new Location(jsonObjectNew));
+}
+
+function Location(jsonArray){
+  this.search_query = '';
+  this.formatted_query = jsonArray[0].display_name;
+  this.latitude = '';
+  this.longitude = '';
+}
+function handleWeather(req, res){
+  // [{
+  //   "forecast": "Partly cloudy until afternoon.",
+  //   "time": "Mon Jan 01 2001"
+  // }]
+}
 
 
 
